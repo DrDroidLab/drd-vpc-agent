89271772411fe5b59c8d5bf8652ca28c0d7851ac
droid-dhruv <dhruv.popuri@drdroid.io>
2025-08-28 17:06:20 +0530

    set the variables separately

--- asset_manager/tasks.py
+++ asset_manager/tasks.py
@@ -22,8 +22,10 @@ def populate_connector_metadata(request_id, connector_name, connector_type, conn
         logger.warning(f"Exception occurred while fetching extractor class for connector: {connector_name}, "
                        f"with error: {e}")
         return False
-    extractor = extractor_class(request_id=request_id, connector_name=connector_name,
-                                api_host=drd_cloud_host, api_token=drd_cloud_api_token, **connector_credentials_dict)
+    extractor = extractor_class(request_id=request_id, connector_name=connector_name, **connector_credentials_dict)
+    # Set API credentials for metadata saving
+    extractor.api_host = drd_cloud_host
+    extractor.api_token = drd_cloud_api_token
     extractor_methods = [method for method in dir(extractor) if
                          callable(getattr(extractor, method)) and method not in dir(SourceMetadataExtractor)]
     for extractor_method in extractor_methods:
@@ -48,8 +50,10 @@ def extractor_async_method_call(request_id, connector_name, connector_type, conn
     drd_cloud_api_token = settings.DRD_CLOUD_API_TOKEN
     
     extractor_class = source_metadata_extractor_facade.get_connector_metadata_extractor_class(connector_type)
-    extractor = extractor_class(request_id=request_id, connector_name=connector_name,
-                                api_host=drd_cloud_host, api_token=drd_cloud_api_token, **connector_credentials_dict)
+    extractor = extractor_class(request_id=request_id, connector_name=connector_name, **connector_credentials_dict)
+    # Set API credentials for metadata saving
+    extractor.api_host = drd_cloud_host
+    extractor.api_token = drd_cloud_api_token
     method = getattr(extractor, extractor_method)
     try:
         method()
