099b4400c805dba2f83499b34db17aad69c86cae
droid-dhruv <dhruv.popuri@drdroid.io>
2025-08-28 16:35:18 +0530

    store metadata in proto db

--- asset_manager/tasks.py
+++ asset_manager/tasks.py
@@ -1,6 +1,7 @@
 import logging
 
 from celery import shared_task
+from django.conf import settings
 
 from drdroid_debug_toolkit.core.integrations.source_metadata_extractor import SourceMetadataExtractor
 from drdroid_debug_toolkit.core.integrations.source_metadata_extractor_facade import source_metadata_extractor_facade
@@ -11,13 +12,18 @@ logger = logging.getLogger(__name__)
 @shared_task(max_retries=3, default_retry_delay=10)
 def populate_connector_metadata(request_id, connector_name, connector_type, connector_credentials_dict):
     logger.info(f"Running populate_connector_metadata for connector: {connector_name} with request_id: {request_id}")
+    
+    drd_cloud_host = settings.DRD_CLOUD_API_HOST
+    drd_cloud_api_token = settings.DRD_CLOUD_API_TOKEN
+    
     try:
         extractor_class = source_metadata_extractor_facade.get_connector_metadata_extractor_class(connector_type)
     except Exception as e:
         logger.warning(f"Exception occurred while fetching extractor class for connector: {connector_name}, "
                        f"with error: {e}")
         return False
-    extractor = extractor_class(request_id=request_id, connector_name=connector_name, **connector_credentials_dict)
+    extractor = extractor_class(request_id=request_id, connector_name=connector_name, 
+                               source=connector_type, api_host=drd_cloud_host, api_token=drd_cloud_api_token, **connector_credentials_dict)
     extractor_methods = [method for method in dir(extractor) if
                          callable(getattr(extractor, method)) and method not in dir(SourceMetadataExtractor)]
     for extractor_method in extractor_methods:
@@ -37,8 +43,13 @@ def extractor_async_method_call(request_id, connector_name, connector_type, conn
                                 extractor_method):
     logger.info(f"Running extractor_async_method_call: {extractor_method} for connector: {connector_name} with "
                 f"request_id: {request_id}")
+    
+    drd_cloud_host = settings.DRD_CLOUD_API_HOST
+    drd_cloud_api_token = settings.DRD_CLOUD_API_TOKEN
+    
     extractor_class = source_metadata_extractor_facade.get_connector_metadata_extractor_class(connector_type)
-    extractor = extractor_class(request_id=request_id, connector_name=connector_name, **connector_credentials_dict)
+    extractor = extractor_class(request_id=request_id, connector_name=connector_name, 
+                               source=connector_type, api_host=drd_cloud_host, api_token=drd_cloud_api_token, **connector_credentials_dict)
     method = getattr(extractor, extractor_method)
     try:
         method()
